ae07ae95e23e1c271dd17396583e1cd80a7958ee
UrvashiRathore <89251583+UrvashiRathore@users.noreply.github.com>
2022-10-18 12:16:12 +0530

    Update 500083262_triangle.java

--- 500083262_triangle.java
+++ 500083262_triangle.java
@@ -3,12 +3,10 @@ import java.util.*;
 public class triangle {
 	public static void main(String args[])
 	{
-		Scanner sc = new Scanner(System.in);
-		int s1 = 0 , s2 = 0 , s3 = 0 ;
+		
+		int s1 = 2 , s2 = 3 , s3 = 3 ;
 		System.out.print("Enter the sides of a triangle :");
-		s1 = sc.nextInt();
-		s2 = sc.nextInt();
-		s3 = sc.nextInt();
+		
 		if( s1 + s2 > s3 && s2+s3 > s1 && s1 + s3 > s2 && s1 > 0 && s2 > 0 && s3 > 0)
 		{
 		if( s1 == s2 && s2 == s3)
